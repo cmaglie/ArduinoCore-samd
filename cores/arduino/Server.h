ffd7453f3f6fd08b5bea35814437c4d354212c2b
Cristian Maglie <c.maglie@arduino.cc>
2016-10-26 19:49:25 +0200

    API: All networking class (Client, Server, UDP, IPAddress)

--- cores/arduino/Server.h
+++ cores/arduino/Server.h
@@ -1,6 +1,5 @@
 /*
-  Server.h - Base class that provides Server
-  Copyright (c) 2011 Adrian McEwen.  All right reserved.
+  Copyright (c) 2016 Arduino LLC.  All right reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -9,22 +8,18 @@
 
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef server_h
-#define server_h
+// Server.h is deprecated, for all future projects use Arduino.h instead
 
-#include "Print.h"
+// This include is added for compatibility, it will be remove on the next
+// major release of the API
+#include "api/Server.h"
 
-class Server : public Print {
-public:
-  virtual void begin() =0;
-};
 
-#endif
